0af624c55613d4964f7449f2f068fd4c9db2dd97
ShadowverseJC <shadowversejc@gmail.com>
2026-03-18 18:11:17 -0400

    user misspell

--- backend/api/register.js
+++ backend/api/register.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
 const { body, validationResult } = require('express-validator');
-const User = require('../models/users');
+const User = require('../models/user');
  
 const JWT_SECRET = process.env.JWT_SECRET || 'secret_key_here';
  
