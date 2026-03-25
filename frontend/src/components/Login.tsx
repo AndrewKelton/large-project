8b30e5e14f3d50fe3e580e96ada3c7e4e687b296
AndrewKelton <andrew7kelton@gmail.com>
2026-03-24 21:08:14 -0400

    add Login functionality

---
+++ frontend/src/components/Login.tsx
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+
+function Login()
+{
+    const [message, setMessage] = useState('');
+    const [username, setUsername] = useState('');
+    const [userPassword, setPassword] = useState('');
+
+    async function doLogin(event:any) : Promise<void>
+    {
+        event.preventDefault();
+        
+        var obj = {Username: username, Password: userPassword};
+        var js = JSON.stringify(obj);
+
+        try {
+            const response = await fetch('http://137.184.68.139:3000/api/login', {method: 'POST', body: js, headers:{'Content-Type': 'application/json'}});
+
+            var res = JSON.parse(await response.text());
+
+            if (!res.token) {
+                setMessage('User/Password combination incorrect');
+            } else {
+                localStorage.setItem('token', res.token);
+                setMessage('');
+                window.location.href = '/userHomePage'; // doesn't exist yet!
+            }
+        } catch(error: any) {
+            alert(error.toString());
+            return;
+        }
+    };
+
+    // set user's login name
+    function handleSetUsername(e: any) : void {
+        setUsername(e.target.value);
+    }
+
+    // set user's password
+    function handleSetPassword(e: any) : void {
+        setPassword(e.target.value);
+    }
+
+    // return form
+    return(
+        <div id="loginDiv">
+            <span id="inner-title">PLEASE LOG IN</span><br />
+            <input type="text" id="username" placeholder="Username"
+                onChange={handleSetUsername} />
+            <input type="password" id="userPassword" placeholder="Password"
+                onChange={handleSetPassword} />
+            <input type="submit" id="loginButton" className="buttons" value = "Do It" onClick={doLogin} />
+            <span id="loginResult">{message}</span>
+        </div>
+    );
+};
+export default Login;
