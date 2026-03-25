1c887ef3937ce19a9c53310d57924875429825d2
AndrewKelton <andrew7kelton@gmail.com>
2026-03-25 12:07:45 -0400

    add home page

---
+++ frontend/src/components/WelcomeMessage.tsx
@@ -0,0 +1,8 @@
+function WelcomeMessage()
+{
+    return (
+        <h1 id="title">Welcome to UCF's Rate My Professor</h1>	
+    );
+};
+
+export default WelcomeMessage;
---
+++ frontend/src/pages/HomePage.tsx
@@ -0,0 +1,13 @@
+import PageTitle from '../components/PageTitle.tsx';
+import WelcomeMessage from '../components/WelcomeMessage.tsx';
+
+const HomePage = () =>
+{
+    return(
+        <div>
+            <PageTitle />
+            <WelcomeMessage />
+        </div>
+    );
+};
+export default HomePage;
