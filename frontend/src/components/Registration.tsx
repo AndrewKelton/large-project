6282dfb6bc51bf169815570886506f7c22f1790a
Adam Betinsky <adambetinskyucf@gmail.com>
2026-03-25 23:34:59 -0400

    Added Button to Process Login

--- frontend/src/components/Registration.tsx
+++ frontend/src/components/Registration.tsx
@@ -1,52 +1,73 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-function Register()
-{
-    const [firstName, setFirstName] = useState('');
-    const [lastName, setLastName] = useState('');
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
+function Register() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-    function handleSetFirstName(e: any) : void {
-        setFirstName(e.target.value);
-    }
+  function handleSetFirstName(e: any): void {
+    setFirstName(e.target.value);
+  }
 
-     function handleSetLastName(e: any) : void {
-        setLastName(e.target.value);
-    }
+  function handleSetLastName(e: any): void {
+    setLastName(e.target.value);
+  }
 
-     function handleSetUsername(e: any) : void {
-        setUsername(e.target.value);
-    }
+  function handleSetUsername(e: any): void {
+    setUsername(e.target.value);
+  }
 
-     function handleSetPassword(e: any) : void {
-        setPassword(e.target.value);
-    }
+  function handleSetPassword(e: any): void {
+    setPassword(e.target.value);
+  }
 
-    return(
-        <div id="registerDiv">
-            <span id="inner-title">REGISTER</span><br />
-            <input type="text" id="firstName" placeholder="First Name"
-                onChange={handleSetFirstName} />
-                        <span>{firstName}</span>
-                        <br></br>
-                        <input type="text" id="lastName" placeholder="Last Name"
-                onChange={handleSetLastName} />
-                        <span>{lastName}</span>
-                                                <br></br>
+  function doRegistration(): void {
+    console.log(firstName, lastName, username, password);
+  }
 
-                        <input type="text" id="username" placeholder="Username"
-                onChange={handleSetUsername} />
-                        <span>{username}</span>
-                                                <br></br>
+  return (
+    <div id="registerDiv">
+      <span id="inner-title">REGISTER</span>
+      <br />
+      <input
+        type="text"
+        id="firstName"
+        placeholder="First Name"
+        onChange={handleSetFirstName}
+      />
+      <span>{firstName}</span>
+      <br></br>
+      <input
+        type="text"
+        id="lastName"
+        placeholder="Last Name"
+        onChange={handleSetLastName}
+      />
+      <span>{lastName}</span>
+      <br></br>
 
-                        <input type="text" id="password" placeholder="Password"
-                onChange={handleSetPassword} />
-                        <span>{password}</span>
-                                                <br></br>
+      <input
+        type="text"
+        id="username"
+        placeholder="Username"
+        onChange={handleSetUsername}
+      />
+      <span>{username}</span>
+      <br></br>
 
-        </div>
-    );
-};
+      <input
+        type="password"
+        id="password"
+        placeholder="Password"
+        onChange={handleSetPassword}
+      />
+      <span>{password}</span>
+      <br></br>
+      <input type="submit" id="registerButton" value="Sign Me Up!" onClick={doRegistration} />
+      <br></br>
+    </div>
+  );
+}
 
 export default Register;
