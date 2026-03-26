591551e0235580f5c49ab00c8718d74c783ac760
Adam Betinsky <adambetinskyucf@gmail.com>
2026-03-25 23:27:10 -0400

    Added text boxes into registration page and got them to display by updating registrationpage.tsx

---
+++ frontend/src/pages/RegistrationPage.tsx
@@ -0,0 +1,12 @@
+import PageTitle from '../components/PageTitle.tsx';
+import Register from '../components/Registration.tsx';
+const RegistrationPage = () =>
+{
+    return(
+        <div>
+            <PageTitle />
+            <Register />
+        </div>
+    );
+};
+export default RegistrationPage;
