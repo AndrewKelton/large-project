5e26abcb42229faa139d701324baa81377284c21
AndrewKelton <andrew7kelton@gmail.com>
2026-03-28 19:55:53 -0400

    handleSelected

--- frontend/src/components/CourseDropdown.tsx
+++ frontend/src/components/CourseDropdown.tsx
@@ -4,7 +4,7 @@ import type { Course } from '../types/index.ts';
 function CourseDropdown() {
 
   const [coursesList, setCoursesList] = useState<Course[]>([]);
-  const [selectedCourse, setSelectedCourse] = useState<Course | null>(null);
+  const [selectedCourseId, setSelectedCourseId] = useState<string>("");
   const [loading, setLoading] = useState<boolean>(false);
 
   useEffect(() => {
@@ -23,11 +23,20 @@ function CourseDropdown() {
     loadCourses();
   }, []); // empty array = run once when component mounts
 
+  function handleSelectedCourse(e: any) : void {
+    setSelectedCourseId(e.target.value);
+  }
+
+  // get full selected course
+  const selectedCourse = coursesList.find(course => course._id === selectedCourseId) ?? null;
+
   return (
     <div>
       {loading && <p>Loading...</p>}
 
-      <select>
+      <h3>Course Dropdown</h3>
+
+      <select value={selectedCourseId} onChange={handleSelectedCourse}>
         <option value="">Select Course</option>
         {coursesList.map(course => (
           <option key={course._id} value={course._id}>
