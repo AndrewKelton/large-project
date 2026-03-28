958a4f759df77f61eb124db5f0384c29e0cf5767
AndrewKelton <andrew7kelton@gmail.com>
2026-03-28 19:42:56 -0400

    add course dropdown

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -2,19 +2,24 @@ import { Link } from 'react-router-dom';
 import PageTitle from '../components/PageTitle.tsx';
 import WelcomeMessage from '../components/WelcomeMessage.tsx';
 import Logout from '../components/Logout.tsx';
+import CourseDropdown from '../components/CourseDropdown.tsx';
 
 const HomePage = () => {
-    const token = localStorage.getItem('token'); // save token
+  const token = localStorage.getItem('token'); // save token
 
-    return(
-        <div>
-            <div style={{ position: 'absolute', top: '1rem', right: '1rem' }}>
-                {!token && <Link to="/auth">Login / Sign Up</Link>}
-            </div>
-            <PageTitle />
-            <WelcomeMessage />
-            {token && <Logout/>}
-        </div>
-    );
+  return(
+    <div>
+      <div style={{ position: 'absolute', top: '1rem', right: '1rem' }}>
+        {!token && <Link to="/auth">Login / Sign Up</Link>}
+      </div>
+
+      <PageTitle />
+      <WelcomeMessage />
+
+      <CourseDropdown />
+
+      {token && <Logout/>}
+    </div>
+  );
 };
 export default HomePage;
