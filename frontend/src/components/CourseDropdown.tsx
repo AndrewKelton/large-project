00581b10ecca5d86c531daa72f934086eaf438e7
AndrewKelton <andrew7kelton@gmail.com>
2026-03-31 13:16:18 -0400

    add searchquery

--- frontend/src/components/CourseDropdown.tsx
+++ frontend/src/components/CourseDropdown.tsx
@@ -9,23 +9,41 @@ function CourseDropdown({ onSelect }: CourseDropdownProps) {
 
   const [coursesList, setCoursesList] = useState<Course[]>([]);
   const [selectedCourseId, setSelectedCourseId] = useState<string>("");
-  const [loading, setLoading] = useState<boolean>(true);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [searchQuery, setSearchQuery] = useState<string>("");
 
   useEffect(() => {
     const loadCourses = async() => {
-      try {
-        const response = await fetch('/api/courses');
-        const data = await response.json();
-        setCoursesList(data);
-      } catch (error: any) {
-        console.error(error);
-      } finally {
-        setLoading(false);
+      // search is empty load all courses
+      if (searchQuery === "") {
+        try {
+          const response = await fetch('/api/courses');
+          const data = await response.json();
+          setCoursesList(data);
+        } catch (error: any) {
+          console.error(error);
+        } finally {
+          setLoading(false);
+        }
       }
+      // search is not empty
+      else {
+        try {
+          const response = await fetch(`/api/courses/search?q=${searchQuery}`);
+          const data = await response.json();
+          setCoursesList(data);
+        } catch (error: any) {
+          console.error(error);
+        }
+        finally {
+          setLoading(false);
+        }
+      }
+
     };
 
     loadCourses();
-  }, []); // empty array = run once when component mounts
+  }, [searchQuery]); // empty array = run once when component mounts
 
   function handleSelectedCourse(e: any) : void {
     const id = e.target.value;
@@ -36,12 +54,19 @@ function CourseDropdown({ onSelect }: CourseDropdownProps) {
     onSelect(course);
   }
 
+  function handleSearchQuery(e: any) : void {
+    const id = e.target.value;
+    setSearchQuery(id);
+  }
+
   return (
     <div>
       {loading && <p>Loading...</p>}
 
       <h3>Course Dropdown</h3>
 
+      <input type="text" value={searchQuery} onChange={handleSearchQuery}></input>
+      {coursesList.length === 0 && searchQuery !== "" && !loading && <p>No courses found.</p>}
       <select value={selectedCourseId} onChange={handleSelectedCourse}>
         <option value="">Select Course</option>
         {coursesList.map(course => (
