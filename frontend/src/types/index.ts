136f3a24ee97e80f6f5ae91b4f0dae8c18a8815d
AndrewKelton <andrew7kelton@gmail.com>
2026-04-05 16:31:49 -0400

    add id field to fetch apis

--- frontend/src/types/index.ts
+++ frontend/src/types/index.ts
@@ -35,7 +35,7 @@ export interface ProfessorRatings {
 
 // A single questionnaire entry as returned by the API
 export interface QuestionnaireEntry {
-  _id?: string;
+  _id: string;
   Question: string;
   Options: {
     A: string | null;
